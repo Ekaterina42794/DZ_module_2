7a1ac2eed3e61f1254dd39a91b6a15b22353931d
Катя <katja48tab42794@gmail.com>
2024-10-16 17:32:24 +0300

    14/10 while

--- module_2_3.py
+++ module_2_3.py
@@ -1,7 +1,8 @@
-
 print('Нули ничто, отрицание недопустимо!')
 
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+print(my_list)
+my_list.remove(0)
 print(my_list)
 index = 0
 while index  < len(my_list):
@@ -10,5 +11,3 @@ while index  < len(my_list):
     if my_list[index] < 0:
         print("цикл закончен",my_list[index],"<0")
         break
-
-
