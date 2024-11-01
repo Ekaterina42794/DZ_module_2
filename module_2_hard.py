2ad8eb508ca5761aaa5bd3702a0f509e6507e9f9
Катя <katja48tab42794@gmail.com>
2024-11-01 21:31:50 +0300

    01/11  "Слишком древний шифр" работает, с сравнением веса списка и кортежа

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,11 +1,12 @@
-print("<<<  Слишком древний шифр  >>>")
+'''print("<<<  Слишком древний шифр  >>>")
 import random
 def stone_1():
     n_ = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
     win = random.choice(n_)
     return win
 a = stone_1()
-print("Для ", a)
+print("Для ", a)'''
+a=20
 result = []
 for i in range(1, 20):
     if i < a:
@@ -23,4 +24,4 @@ print(type(result),"пароль", *result, "=", result.__sizeof__(),)
 result_kortej=tuple([result]) # перевод списка в кортеж !!!
 print(type(result_kortej),result_kortej, "=", result_kortej.__sizeof__(),"Кортеж занимает мало места и неизменяем")
 
-
+'''откуда берется запятая при выввводе кортежа при малом весе?????'''
