d34b03e5209b4479202f77fa9b608d547d78574d
Катя <katja48tab42794@gmail.com>
2024-11-01 14:07:55 +0300

    01/11 пароль

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,15 +1,25 @@
 import random
 
-def lottery_():
-    tickets = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    win = random.choice(tickets)
+result = []
+
+
+def stone_1():
+    n_ = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+    win = random.choice(n_)
     return win
 
-print(lottery())
 
+a = stone_1()
+print("Для ", a)
+for i in range(1, 20):
+    if i < a:
+        for j in range(1, 20):
+            while j < a:
+                y = i + j
+                if stone_1() % y == 0:
+                    result.append(j)
+                continue
+            else:
+                result.remove(i)
 
-
-
-
-
-
+print("пароль", result())
