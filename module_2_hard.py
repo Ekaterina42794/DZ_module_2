635b5911d112a190049107943f7ac279faffe420
Катя <katja48tab42794@gmail.com>
2024-11-01 15:25:05 +0300

    01/11 задача на подбор пароля работает

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,25 +1,20 @@
 import random
-
-result = []
-
-
 def stone_1():
     n_ = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
     win = random.choice(n_)
     return win
-
-
 a = stone_1()
 print("Для ", a)
+result = []
 for i in range(1, 20):
     if i < a:
+        '''print("i=",i)'''
         for j in range(1, 20):
-            while j < a:
-                y = i + j
-                if stone_1() % y == 0:
+            if j < a:
+                n=i+j
+                if a % n == 0 and i<j:
+                    result.append(i)
                     result.append(j)
+                    '''print("j=", j)'''
                 continue
-            else:
-                result.remove(i)
-
-print("пароль", result())
+print("пароль", result)
